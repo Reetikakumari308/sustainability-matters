888e24fef58663aeed88c77f7f0bc0de24ead665
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2025-05-27 15:06:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://127.0.0.1:5500',  // Frontend URL
+  origin: 'https://sustainability-matters-1.onrender.com',  // Frontend URL
   credentials: true
 }));
 app.use(express.json());
